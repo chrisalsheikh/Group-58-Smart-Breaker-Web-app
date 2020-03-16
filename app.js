7c873ffe996c3534ae89b2c2368988ac5b1a6e2b
chrisalsheikh <chrisalsheikh97@gmail.com>
2020-03-15 22:50:01 -0700

    toggle LED over network using websockets

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-const http = require("http"); // require http module
-const fs = require("fs"); // require file system module
-const port = 3000;
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var Gpio = require("onoff").Gpio;
 
-const server = http.createServer(handler);
+var LED = new Gpio(4, 'out');
+// var blinkInterval = setInterval();
 
-// handler function creates server
-function handler(req, res) {
-  fs.readFile("./index.html", function(err, data) {
-    // read file index.html
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      return res.end("404 not found");
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    res.end();
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("buttonClick", function(data) {
+    console.log(data);
+    blinkLED();
   });
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
+
+function blinkLED() { //function to start blinking
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
 }
 
-server.listen(port, function(error) {
-  if (error) {
-    console.log("Something went Wrong", error);
-  } else {
-    console.log("Server is listening on port " + port);
-  }
-});
